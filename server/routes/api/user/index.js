fa2dbfa158d6d1599ef8c798c2fc9c0be6f117ff
Apple <thirdessential@gmail.com>
2020-06-30 20:52:53 +0530

    fetch user api

--- server/routes/api/user/index.js
+++ server/routes/api/user/index.js
@@ -8,6 +8,10 @@ router.post('/setpassword', controller.setpassword)
 router.post('/verify', controller.verify)
 router.get('/delete/:id', controller.deleteUser)
 router.get('/count', controller.count)
+router.post('/update/:id', controller.updateUser)
+router.get('/view/:id', controller.viewUser)
+
+
 
 
 
