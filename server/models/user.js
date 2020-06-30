b5aa206b018c37cbda91b86f12d0236c7ab511f2
Apple <thirdessential@gmail.com>
2020-06-30 20:52:05 +0530

    user model changes

--- server/models/user.js
+++ server/models/user.js
@@ -16,7 +16,7 @@ const User = new Schema({
     phoneNumber:String,
     blocked: {type: Boolean, default: false},
     verified: {type: Boolean, default: false},
-
+    customFields:[]
 }, schemaOptions)
 
 
