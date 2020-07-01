9fefc8893f0f4a2127abf0595ff51e7447a35fc8
Apple <thirdessential@gmail.com>
2020-07-01 20:36:22 +0530

    edit matter

--- src/App/components/AddEditMatter/DynamicFeilds/index.js
+++ src/App/components/AddEditMatter/DynamicFeilds/index.js
@@ -1,41 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import { Form,Button, Row , Col } from "react-bootstrap";
+import api from '../../../../resources/api'
 
-
-
+let response = {}
+let optns = null
 class DynamicFeilds extends React.Component {
   constructor(props){
     super(props)
+    this.state = {
+      InputList : this.props.InputList
+    }
   }
-  
-
+ 
     render(){
-      
     return (
         <div >
             {
-            this.props.InputList.map((val, idx)=> {
+            this.state.InputList.map((val, idx)=> {
                   let inputId = `input-${idx}`
                
                   return (
                     <div key={idx}>
                     <Row>
                       <Col>
-                            <Form.Group controlId="formGroupRelationship">
+                            <Form.Group controlId={idx}>
                                 <Form.Label>Relationship</Form.Label>
-                                <Form.Control name='Relationship' type="text" placeholder="Relationship" 
-                                value={val['Relationship']} onChange={this.props.change}/>
+                                <Form.Control name='Relationship' type="text" placeholder="Relationship"  onChange={this.props.change}/>
                             </Form.Group>
                      </Col>
                      <Col>
-                          <Form.Group controlId="exampleForm.ControlSelect1">
+                          <Form.Group controlId={idx}>
                           <Form.Label>Contact</Form.Label>
                           <Form.Control as="select">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
+                            {this.props.option}
                          </Form.Control>
                          </Form.Group>
                     </Col>
