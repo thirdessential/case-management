7448c079687f96439b9be6fc84990ec4272a6eb7
Apple <thirdessential@gmail.com>
2020-07-02 20:12:16 +0530

    added the apis

---
+++ server/routes/api/tasks/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const controller = require('./tasks.controller')
+
+
+router.post("/create", controller.createTask)
+router.get("/delete/:id", controller.deleteTask)
+router.get("/showall", controller.showAllTasks)
+router.post("/edit/:id", controller.editTasks)
+router.get("/view/:id", controller.viewTasks)
+
+
+module.exports = router
