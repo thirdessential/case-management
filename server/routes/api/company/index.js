7acf48534b16e8536e26f36c686fb447e3d9f889
Anas M.I <anas3rde@gmail.com>
2020-07-06 21:36:29 +0530

    route to fetch contacts of specific users

--- server/routes/api/company/index.js
+++ server/routes/api/company/index.js
@@ -6,4 +6,6 @@ router.post("/create", controller.createCompany)
 router.get("/delete/:id", controller.deleteCompany)
 router.get("/showall", controller.showAll)
 router.get("/view/:id", controller.view)
+router.get("/viewforuser/:id", controller.viewSpecific)
+
 module.exports = router
