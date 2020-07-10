6f9b5fedc2cebc2804d087b0bdba91061c8947df
Anas M.I <anas3rde@gmail.com>
2020-07-10 19:59:25 +0530

    bug fix

--- server/routes/api/contacts/contacts.controller.js
+++ server/routes/api/contacts/contacts.controller.js
@@ -84,3 +84,30 @@ exports.viewSpecific = (req, res) => {
 
         })
 }
+
+
+//upload
+exports.upload= (req, res) => {
+console.log(req.params.id)
+    Contact.findByIdAndUpdate(req.params.id, {$set:{image:req.file.path}} ).
+        then(data => {
+            res.status(200).json({status: true, message:"image uploaded", data})
+
+        }).catch(error => {
+        res.status(200).json({status: false, message:error})
+
+        })
+}
+
+//Edit contact
+exports.editContact = (req, res) => {
+
+    Contact.findByIdAndUpdate(req.params.id, req.body, {new: true}).
+        then(data => {
+            res.status(200).json({status: true, message:"Contact updated", data})
+
+        }).catch(error => {
+        res.status(400).json({status: false, message:error})
+
+        })
+}
