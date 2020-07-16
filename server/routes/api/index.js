952e1521f0ef9aa59e9a35c660270afa0b1d12c4
Anas M.I <anas3rde@gmail.com>
2020-07-16 20:22:52 +0530

    acotovty route added

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -12,7 +12,10 @@ const contact = require("./contacts")
 const matter = require("./matters")
 const tasks = require("./tasks")
 const calendar = require("./calendar")
-
+const activity = require("./activities")
+const document = require("./documents")
+const billing = require("./billing")
+const account = require("./accounts")
 router.use('/contactus', contactus)
 router.use('/blogs', blogs)
 router.use('/features', features)
@@ -21,12 +24,15 @@ router.use('/auth', auth)
 router.use('/matter', matter)
 router.use('/company', company)
 router.use('/contact', contact)
+router.use('/billing', billing)
 // router.use('/user', authMiddleware)
 router.use('/user', user)
 router.use('/tasks', tasks)
 router.use("/calendar", calendar)
-
+router.use("/activity", activity)
 router.use('/admin', admin)
+router.use('/document', document)
+router.use('/account', account)
 
 
 module.exports = router
