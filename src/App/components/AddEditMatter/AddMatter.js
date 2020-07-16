ec24751d916001589951175feef796b9513c8256
Anas M.I <anas3rde@gmail.com>
2020-07-16 20:33:08 +0530

    add matter

--- src/App/components/AddEditMatter/AddMatter.js
+++ src/App/components/AddEditMatter/AddMatter.js
@@ -51,14 +51,7 @@ class AddEditMatter extends React.Component{
     
    
     optns = contacts.data.data.map((value, index)=>{
-      if(index == 0){
-        const temp = this.state
-        temp.relatedContacts.contact = value._id
-        temp.client = value._id
 
-        this.setState(temp)
-
-      }
       return <option id={index}>{value.firstName}</option>
      })
    
@@ -66,7 +59,9 @@ class AddEditMatter extends React.Component{
     customFields = res.data.data.customFields.map((value, index)=>{
       return <Form.Group key={index} controlId={index}>
               <Form.Label>{value.name}</Form.Label>
-              <Form.Control required={value.required} name={value.name} type={value.type} placeholder={value.name} onChange={this.handleCustom}/>
+              <Form.Control required={value.required} 
+              name={value.name} type={value.type}  
+              onChange={this.handleCustom} />
              </Form.Group>
     })
     this.setState({optns : optns, customFields : customFields})
@@ -91,13 +86,12 @@ class AddEditMatter extends React.Component{
       });
     }else  if ((this.state.client ==="" ||this.state.client ===undefined) ) {
       return notification.warning({
-        message: "Please add a matter description",
+        message: "Please add a Contact",
       });
     }else{
       console.log("all good")
        const data = this.state
         data.customFields = customData
-        data.client = contacts.data.data[clientId]._id
         data.userId = this.props.userId
         console.log(data)
        if(this.state.editMode){
@@ -118,7 +112,7 @@ class AddEditMatter extends React.Component{
 
     const addFeild=() =>{
       let list = this.state.relatedContacts
-      list.push({relationship : "", Contact : "", billThis : "", id: ""})
+      list.push({relationship : "", contact : "", billThis : "", id: ""})
       this.setState({relatedContacts : list})
       let newState= this.state
       newState.Relation = this.state.relatedContacts
@@ -127,11 +121,13 @@ class AddEditMatter extends React.Component{
   
   const handleChange = (e) => {
     e.persist()
-    this.setState(st=>({...st,[e.target.name]:e.target.value}))
+ 
     if(e.target.name==="client"){
-      clientId = e.target.selectedIndex
+       this.setState(st=>({...st,[e.target.name]: contacts.data.data[e.target.selectedIndex]}))
+    }else{
+      this.setState(st=>({...st,[e.target.name]:e.target.value}))
     }
-    console.log(clientId)
+
    
     console.log(this.state)
   }
@@ -189,7 +185,7 @@ class AddEditMatter extends React.Component{
           <Form.Group controlId="exampleForm.ControlSelect1">
             <Form.Label>Client</Form.Label>
               <Form.Control as="select" name="client" onChange={handleChange}>
-                
+                <option>Select a contact</option>
                 {optns}
               </Form.Control>
             </Form.Group>
@@ -210,10 +206,12 @@ class AddEditMatter extends React.Component{
                 <Form.Label>Practise Area</Form.Label>
                 <Form.Control as="select" name="practiseArea" onChange={handleChange} defaultValue={editRes.practiseArea}>
                   <option>{editRes.practiseArea}</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
+                  <option>Attorney</option>
+                  <option>Administrative</option>
+                  <option>Business</option>
+                  <option>Family</option>
+                  <option>Imployment</option>
+                  <option>Tax</option>
                 </Form.Control>
               </Form.Group>
               <Form.Group controlId="exampleForm.ControlSelect1">
