747ae79a7bfb5dbd68501eafa0748bbd669a53ff
Anas M.I <anas3rde@gmail.com>
2020-07-28 16:39:40 +0530

    record payment

--- src/App/components/Recordpayment/recordPayment.js
+++ src/App/components/Recordpayment/recordPayment.js
@@ -5,65 +5,180 @@ import api from '../../../resources/api'
 import { connect } from 'react-redux'
 
 let optns = null
+let contacts = {}
 class Record extends React.Component{
    constructor(props){
        super(props)
        this.state = {
            data : {
                client : "",
-               destination : "",
+            //   destination : "",
                paymentDate : "" , 
                source : "" ,
+               userId : this.props.userId
 
-           }
+           },
+           clientId : "",
+           clientData :"",
+           tableData : [],
+           payment :[],
+           total : 0,
+           fromTotal  : true
        }
    }
    componentDidMount(){
      api.get('/contact/viewforuser/'+this.props.userId).then((res)=>{
-        optns = res.data.data.map((value, index)=>{
-
-            return <option id={index}>{value.firstName}</option>
+         contacts = res.data.data
+         optns = res.data.data.map((value, index)=>{
+          return <option id={index}>{value.firstName + " " + value.lastName}</option>
            })
-     }).then(()=>{this.setState({options : optns})})
+     }).then(()=>{
+         this.setState({options : optns})
+        })
     
     
    }
     render(){
+        const handletotalBalance = (e) => {
+            e.persist()
+            const {id , value} = e.target
+            let newState = this.state
+            console.log(e.target)
+            newState.payment[0] = value
+            this.setState(newState)
+            this.setState({fromTotal : true})
+            console.log(this.state.payment)
+        }
+        const handlePayment = (e) => {
+            e.persist()
+            const {id , value} = e.target
+            let newState = this.state
+            console.log(e.target)
+            newState.payment[id] = value
+            this.setState(newState)
+            console.log(this.state.payment)
+        }
         const handleChange = (e) => {
             e.persist()
             const { name, id, value} = e.target
             let newData = this.state.data
-            newData[name] = value
-            this.setState({data : newData})   
-            console.log(this.state.data)
+          
+            if(name === "client"){
+                newData[name] = contacts[e.target.selectedIndex]
+                if(e.target.selectedIndex !=0){
+                    this.setState({clientId : contacts[e.target.selectedIndex - 1]._id })
+                    console.log( contacts[e.target.selectedIndex - 1]._id)
+
+                    api.get('/billing/bill/viewforcontact/'+this.props.userId+'/'+ contacts[e.target.selectedIndex - 1]._id).then((res)=>{
+                        this.setState({clientData : res.data.data})
+                        console.log(res.data.data)
+                        console.log("foo foo ")
+                        let tableData = []
+                        let paidBills = []
+                        let unpaidBills = []
+                        res.data.data.map((value , index)=>{
+                                let pay = this.state.payment
+                                pay[index] = 0.00
+                                this.setState({payment : pay , total : this.state.total  + parseFloat(value.balance)})
+
+                            //    const issueDate = today.getDate() + "/" + today.getMonth() + "/" + today.getFullYear()
+                                const temp = {
+                                  id : value._id,
+                                  status : value.status,
+                                  lastSeen : value.lastSeen ? value.lastSeen : "--",
+                                  dueDate : value.dueDate,
+                                  invoiceID : value.invoiceId ? value.invoiceId : "-" ,
+                                  dueIn : value.dueDate.substring(0,10),
+                                  matter : value.matter,
+                                  openBalace : value.balance,
+                                  issueDate : value.issueDate.substring(0,10) ,
+                                  openBalance : value.balance,
+                                  payment : <Form>
+                                      <Row>
+                                          <Col md="10">
+                                            <Form.Group controlId={index}>
+                                              
+                                                <Form.Control 
+                                                style = {{height : "60%",}}
+                                                type="text" 
+                                                key={index}
+                                                name="payment" 
+                                                placeholder = "$0.00"
+                                                onChange={handlePayment}/>
+                                            </Form.Group>
+                                          </Col>
+                                      </Row>
+                                  </Form>
+                                  
+                                }
+                                if(value.status=="Paid"){
+                                  paidBills.push(temp)
+                                }
+                                if(value.status=="Unpaid"){
+                                  unpaidBills.push(temp)
+                                }
+                                tableData.push(temp)
+                              })
+                              this.setState({tableData :  tableData, paidBills : paidBills , unpaidBills : unpaidBills})
+                    }).catch((err)=>{
+                        console.log(err)
+                    })
+                }else{
+                    this.setState({clientId : ""})
+                }
+               
+            }else{
+                newData[name] = value
+            }
+            this.setState({data : newData})  
+             
         }
         const handleSubmit = (e) =>{
-            if(this.state.data.client === ""){
+            if(this.state.data.client === "" || this.state.data.source ===  "Select a client"){
                 notification.error({message : "Please select a client"})
-            }else if(this.state.data.source === ""){
+            }else if(this.state.data.source === "" || this.state.data.source ===  "Select a Source"){
                 notification.error({message : "Please select a source"})
-            }else if(this.state.data.destination === ""){
+            } /* else if(this.state.data.destination === "" || this.state.data.destination === "Select a Destination"){
                 notification.error({message : "Please select a destination"})
-            }else if(this.state.data.paymentDate === ""){
+            }*/else if(this.state.data.paymentDate === ""){
                 notification.error({message : "Please select a payment date"})
             }else{
-                this.props.history.goBack()
+                this.state.clientData.map((value,id)=>{
+        
+                    const data = value
+                    data.lastSeen = new Date()
+                    if(this.state.fromTotal){
+                        const bal = this.state.payment[0]
+                       if( parseFloat(data.balance) - parseFloat(bal) > 0){
+                        data.balance = parseFloat(data.balance) - parseFloat(bal)
+                       }
+                    }else {
+                        const bal = this.state.payment[id]
+                        data.balance = parseFloat(data.balance) - parseFloat(bal)
+                    }
+                   
+                    console.log(data)
+                    
+                    api.post('/billing/bill/edit/'+value._id, data ).then((res)=>{
+                        console.log(res)
+                        notification.success({message : "Bill Recorded"})
+                    }).catch((err)=>{
+                        console.log(err)
+                        notification.error({message : "Failure "})
+                   })
+                })
+               
+               api.post('/billing/create',this.state.data).then((res)=>{
+                   console.log(res)                 
+                   this.props.history.goBack()
+               }).catch((err)=>{
+            
+               })
+               
             }
+        
         }
-        const dataSource = [
-            {
-              key: '1',
-              name: 'Mike',
-              age: 32,
-              address: '10 Downing Street',
-            },
-            {
-              key: '2',
-              name: 'John',
-              age: 42,
-              address: '10 Downing Street',
-            },
-          ];
+        
           
           const columns = [
             {
@@ -91,14 +206,14 @@ class Record extends React.Component{
               dataIndex: 'status',
               key: 'status',
             },
-           
+           /*
             {
                 title: 'Intrest',
                 dataIndex: 'intrest',
                 key: 'intrest',
               },
              
-            
+            */
              
               {
                 title: 'Open Balance',
@@ -116,7 +231,7 @@ class Record extends React.Component{
         const title = <div style={{"display": "flex", "flex-wrap": "wrap" }}>
                         <div className="mr-4">
                             <p style={{fontWeight : "bold"}}>Total open balance</p>
-                            <p style={{fontWeight : "bold", "float": "right", "font-size": "17px"}}>₹0.00</p>
+        <p style={{fontWeight : "bold", "float": "right", "font-size": "17px"}}>${this.state.total}</p>
                         </div>
                         <div>
                             <Form className="pt-0">
@@ -126,10 +241,11 @@ class Record extends React.Component{
                                     <Form.Label style={{fontWeight : "bold"}}>Payment Amount</Form.Label>
                                     <Form.Control
                                     style={{height : "38px"}}
+                             
                                     required
                                     name="paymentAmount"
                                     type="text"
-                                    onChange = { handleChange }
+                                    onChange = { handletotalBalance }
                                     placeholder="0.00"
                                 //    onChange={handleChange}
                                     />
@@ -148,7 +264,7 @@ class Record extends React.Component{
                             <div>
 
                                 <Card bodyStyle={{"padding": "0px"}} className="overflow-auto">
-                                    <Table dataSource={dataSource} columns={columns} />
+                                    <Table dataSource={this.state.tableData} columns={columns} />
                                 </Card> 
 
 
@@ -200,7 +316,10 @@ class Record extends React.Component{
                               as="select"
                               onChange = { handleChange }>
                                 <option>Select a Source</option>
-                                <option>Source</option>
+                                <option>Card Payment</option>
+                                <option>Cash Payment</option>
+                                <option>Net Banking</option>
+                                <option>Other</option>
                             </Form.Control>
                         </Form.Group>
                      </Col>
