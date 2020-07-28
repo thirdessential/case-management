bbbd57cb3009f3bda1b9ede594af096647118635
Anas M.I <anas3rde@gmail.com>
2020-07-28 16:40:26 +0530

    billing

--- src/App/containers/Billings/billing.js
+++ src/App/containers/Billings/billing.js
@@ -1,33 +1,73 @@
 import React from 'react';
-import { Tabs, Card, Table, Button } from 'antd';
+import { Tabs, Card, Table, Button, Popconfirm, message } from 'antd';
+import { Form } from 'react-bootstrap';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
+import api from '../../../resources/api';
+import { connect } from 'react-redux'
 const { TabPane } = Tabs;
 
-export default class billing extends React.Component {
+class billing extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      tableData : [],
+      status : false
+    }
+  }
   handelBills = (type) => {
     if (type === 'record') {
       this.props.history.push('/manage/billing/record');
-    } else if (type === 'expense') {
     }
   };
 
+  cancel(e) {
+    message.error('Canceled');
+  }
+
+  handelAction = (e) => {
+    this.setState({
+      status : true
+    })
+  }
+
+  componentDidMount(){
+    api.get('/tasks/list/showall').then((res)=> {
+      console.log(res.data)
+    })
+    api.get('/billing/bill/viewforuser/'+this.props.userId).then((res)=>{
+      console.log(res.data.data)
+      let tableData = []
+      let paidBills = []
+      let unpaidBills = []
+      res.data.data.map((value , index)=>{
+     
+    //    const issueDate = today.getDate() + "/" + today.getMonth() + "/" + today.getFullYear()
+        const temp = {
+          lastSeen : value.lastSeen ? value.lastSeen.substring(0,10) : "-",
+          status : value.status,
+          dueDate : value.dueDate.substring(0,10),
+          id : value.invoiceId,
+          client : value.client,
+          matter : value.matter,
+          issueDate : value.issueDate.substring(0,10) ,
+          balance : value.balance
+        }
+        if(value.status=="Paid"){
+          paidBills.push(temp)
+        }
+        if(value.status=="Unpaid"){
+          unpaidBills.push(temp)
+        }
+        tableData.push(temp)
+      })
+      this.setState({tableData :  tableData, paidBills : paidBills , unpaidBills : unpaidBills})
+    })
+  }
+
   render() {
     const callback = () => {};
-    const dataSource = [
-      {
-        key: '1',
-        name: 'Mike',
-        age: 32,
-        address: '10 Downing Street',
-      },
-      {
-        key: '2',
-        name: 'John',
-        age: 42,
-        address: '10 Downing Street',
-      },
-    ];
+   
 
     const columns = [
       {
@@ -67,10 +107,72 @@ export default class billing extends React.Component {
       },
       {
         title: 'Balance',
-        dataIndex: 'Balance',
-        key: 'Balance',
+        dataIndex: 'balance',
+        key: 'balance',
       },
     ];
+
+    const unpaidColumns= [
+      {
+        title: 'Last Seen',
+        dataIndex: 'lastSeen',
+        key: 'lastSeen',
+      },
+      {
+        title: 'Status',
+        dataIndex: 'status',
+        key: 'status',
+      },
+      {
+        title: 'Due Date',
+        dataIndex: 'dueDate',
+        key: 'dueDate',
+      },
+      {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: 'Client',
+        dataIndex: 'client',
+        key: 'client',
+      },
+      {
+        title: 'Matter',
+        dataIndex: 'matter',
+        key: 'matter',
+      },
+      {
+        title: 'Issue Date',
+        dataIndex: 'issueDate',
+        key: 'issueDate',
+      },
+      {
+        title: 'Balance',
+        dataIndex: 'balance',
+        key: 'balance',
+      },
+      {
+        title: 'Action',
+        dataIndex: 'status',
+        key: '7',
+        render: (_, record) => {
+          return (
+            <Popconfirm
+              title="Mark as Paid"
+              onConfirm={() => this.handelAction(record._id)}
+              onCancel={this.cancel}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Form.Check type="checkbox"  />
+            </Popconfirm>
+          );
+        },
+      },
+    ];
+
     const exportPDF = () => {
       const unit = 'pt';
       const size = 'A4'; // Use A1, A2, A3 or A4
@@ -81,7 +183,7 @@ export default class billing extends React.Component {
 
       doc.setFontSize(15);
 
-      const title = 'Activity';
+      const title = 'Bills';
       const headers = [
         [
           'Last Seen',
@@ -94,14 +196,13 @@ export default class billing extends React.Component {
           'Balance',
         ],
       ];
-
-      let data = [];
-      /*
-            state.tableData.map((val, index)=>{
-              const td= [val.firstName, val.billingCustomRate , val.emailAddress]
+    
+      let data = 
+            this.state.tableData.map((val, index)=>{
+              const td= [val.lastSeen, val.status , val.dueDate , val.id ,val.client , val.matter , val.issueDate , val.balance]
               data.push(td)
             })
-           */
+     
 
       let content = {
         startY: 50,
@@ -111,7 +212,7 @@ export default class billing extends React.Component {
 
       doc.text(title, marginLeft, 40);
       doc.autoTable(content);
-      doc.save('contact.pdf');
+      doc.save('Bill.pdf');
     };
 
     return (
@@ -131,17 +232,21 @@ export default class billing extends React.Component {
           }
         >
           <Tabs defaultActiveKey="4" onChange={callback}>
-            <TabPane tab="Draft" key="1">
-              <Table dataSource={dataSource} columns={columns} />;
+            {
+              /* 
+               <TabPane tab="Draft" key="1">
+              <Table dataSource={this.state.tableData} columns={columns} />;
             </TabPane>
-            <TabPane tab="Paid" key="2">
-              <Table dataSource={dataSource} columns={columns} />;
+              */
+            }
+           <TabPane tab="All" key="4">
+              <Table dataSource={this.state.tableData} columns={columns} />
             </TabPane>
             <TabPane tab="Unpaid" key="3">
-              <Table dataSource={dataSource} columns={columns} />;
+              <Table dataSource={this.state.unpaidBills} columns={unpaidColumns} />
             </TabPane>
-            <TabPane tab="All" key="4">
-              <Table dataSource={dataSource} columns={columns} />;
+            <TabPane tab="Paid" key="2">
+              <Table dataSource={this.state.paidBills} columns={columns} />
             </TabPane>
           </Tabs>
         </Card>
@@ -149,3 +254,9 @@ export default class billing extends React.Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  userId: state.user.token.user._id
+});
+
+export default connect(mapStateToProps)(billing)
