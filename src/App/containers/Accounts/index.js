715dbeb799bb4f73eaf141d7adc0c51f4fb5a07f
Anas M.I <anas3rde@gmail.com>
2020-08-25 22:05:27 +0530

    index change

--- src/App/containers/Accounts/index.js
+++ src/App/containers/Accounts/index.js
@@ -1,30 +1,52 @@
 import React, {useState, useEffect} from  'react'
-import { Table, notification, Button } from 'antd';
+import { Table, notification, Button, Popconfirm, Spin } from 'antd';
 import { useHistory } from 'react-router-dom';
 import api from '../../../resources/api'
+import { useSelector } from 'react-redux'
 
 const Accounts = () => {
     const history = useHistory()
     const [state, setState] = useState([])
+    const [Loading, setLoading] = useState(true)
+    const userId = useSelector((state) => state.user.token.user._id);
     
-    useEffect(() => {
+    const fetchAccount = ( ) =>{
       api
-      .get("/account/showall")
+      .get('/account/viewforuser/' + userId)
       .then((res) => {
-        setState([...state, ...res.data.data])
+        console.log(res)
+        const tableData = []
+        res.data.data.map((value, index) => {
+          console.log(value.defaultAccount)
+          const data = {
+            _id : value._id,
+            key : index,
+            accountName : value.accountName,
+            currency : value.currency,
+            openingBalance : value.openingBalance,
+            type :  value.defaultAccount ? "Yes" : "No"
+          }
+          tableData.push(data)
+        })
+        setState(tableData)
+        setLoading(false)
       })
       .catch((err) => {
         console.log(err); 
       });
+    }
+    useEffect(() => {
+      fetchAccount()
     }, []);
 
     const handleDelete = (record) =>{
         console.log(record)
         api.get('/account/delete/'+record._id)
         .then((res)=>{
+          fetchAccount()
           notification.success({message : "Account Deleted !"})
           setTimeout(() => {
-            window.location.reload()
+           // window.location.reload()
           }, 1000);
         })
         .catch((err)=>{
@@ -56,9 +78,18 @@ const Accounts = () => {
           key: "_id",
           render:(_,record)=>{
               return (
-                  <Button variant='danger' onClick={()=>handleDelete(record)}>
+                <Popconfirm
+                    title="Are you sure delete this Account?"
+                    onConfirm={()=>handleDelete(record)}
+                    onCancel={()=>{}}
+                    okText="Yes"
+                    cancelText="No"
+                  >
+                    <Button  danger>
                       Delete
                   </Button>
+                  </Popconfirm>
+                  
                   )
               }
         },
@@ -68,7 +99,7 @@ const Accounts = () => {
             key: "_id",
             render:(_,record)=>{
                 return (
-                    <Button variant='danger' onClick={() => { history.push('/edit/accounts', record._id)}} >
+                    <Button  onClick={() => { history.push('/edit/accounts', record._id)}} >
                         Edit
                     </Button>
                 )
@@ -78,11 +109,14 @@ const Accounts = () => {
 
     return(
         <>
-            <div className='p-2 '>
+        <Spin size = "large" spinning={Loading}>
+          <div className='p-2 '>
                 <Button className='ml-auto' color='success'>Export</Button>
                 <Button className='ml-auto' color='success' style={{float : "right"}} onClick={() => { history.push('/add/accounts')}} >Add</Button>
             </div>
             <Table columns={columns} dataSource={state}/>
+        </Spin>
+            
         </>
     )
 }
