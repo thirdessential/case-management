52f7bb869826f93385676dc30d931fd9885db8cc
Anas M.I <anas3rde@gmail.com>
2020-08-30 18:40:15 +0530

    billing

--- src/App/containers/Billings/billing.js
+++ src/App/containers/Billings/billing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs, Card, Table, Button, Popconfirm, message } from 'antd';
+import { Tabs, Card, Table, Button, Popconfirm, message, notification, Spin } from 'antd';
 import { Form } from 'react-bootstrap';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
@@ -12,7 +12,11 @@ class billing extends React.Component {
     super()
     this.state = {
       tableData : [],
-      status : false
+      draftBills : [],
+      unpaidBills : [],
+      paidBills : [],
+      status : false,
+      loading : true
     }
   }
   handelBills = (type) => {
@@ -25,33 +29,30 @@ class billing extends React.Component {
     message.error('Canceled');
   }
 
-  handelAction = (e) => {
-    this.setState({
-      status : true
-    })
-  }
+  
 
   componentDidMount(){
-    api.get('/tasks/list/showall').then((res)=> {
-      console.log(res.data)
-    })
+   
     api.get('/billing/bill/viewforuser/'+this.props.userId).then((res)=>{
       console.log(res.data.data)
       let tableData = []
       let paidBills = []
       let unpaidBills = []
+      let draftBills = []
       res.data.data.map((value , index)=>{
      
     //    const issueDate = today.getDate() + "/" + today.getMonth() + "/" + today.getFullYear()
         const temp = {
+          key : index ,
+          _id : value._id,
           lastSeen : value.lastSeen ? value.lastSeen.substring(0,10) : "-",
           status : value.status,
           dueDate : value.dueDate.substring(0,10),
-          id : value.invoiceId,
-          client : value.client,
-          matter : value.matter,
+          id : value.invoiceId ? value.invoiceId : '-' ,
+          client : value.client ? value.client.firstName + " " + value.client.lastName : "-",
+          matter : value.matter ? value.matter.matterDescription : "-",
           issueDate : value.issueDate.substring(0,10) ,
-          balance : value.balance
+          balance : parseFloat(value.balance).toFixed('2')
         }
         if(value.status=="Paid"){
           paidBills.push(temp)
@@ -59,109 +60,84 @@ class billing extends React.Component {
         if(value.status=="Unpaid"){
           unpaidBills.push(temp)
         }
+        if(value.status=="draft"){
+          draftBills.push(temp)
+        }
         tableData.push(temp)
       })
-      this.setState({tableData :  tableData, paidBills : paidBills , unpaidBills : unpaidBills})
+      this.setState({
+        tableData :  tableData,
+         paidBills : paidBills ,
+          unpaidBills : unpaidBills,
+           draftBills: draftBills,
+            loading : false})
     })
   }
 
   render() {
     const callback = () => {};
-   
+    const handelAction = (record, type) => {
+      const data = record
+      delete data.matter
+      delete data.client
+      if(type === "fromDraft"){
+        data.status = "Unpaid"
+        
+      }else
+      if(type === "fromUnpaid"){
+        data.status = "Paid"
+      }
+      api.post('/billing/bill/edit/'+record._id , data).then((res)=>{
+        console.log(res)
+        if(type=="fromDraft"){
+          const newState = this.state
+          newState.draftBills.splice(record.key, 1)
+          newState.unpaidBills.push(res.data.data)
+        }
+        if(type=="fromUnpaid"){
+          const newState = this.state
+          newState.unpaidBills.splice(record.key, 1)
+          newState.paidBills.push(res.data.data)
+        }
 
-    const columns = [
-      {
-        title: 'Last Seen',
-        dataIndex: 'lastSeen',
-        key: 'lastSeen',
-      },
-      {
-        title: 'Status',
-        dataIndex: 'status',
-        key: 'status',
-      },
-      {
-        title: 'Due Date',
-        dataIndex: 'dueDate',
-        key: 'dueDate',
-      },
-      {
-        title: 'ID',
-        dataIndex: 'id',
-        key: 'id',
-      },
-      {
-        title: 'Client',
-        dataIndex: 'client',
-        key: 'client',
-      },
-      {
-        title: 'Matter',
-        dataIndex: 'matter',
-        key: 'matter',
-      },
-      {
-        title: 'Issue Date',
-        dataIndex: 'issueDate',
-        key: 'issueDate',
-      },
-      {
-        title: 'Balance',
-        dataIndex: 'balance',
-        key: 'balance',
-      },
-    ];
+        notification.success({message : "Success"})
+        setTimeout(()=>{
+        //  window.location.reload()
+          this.componentDidMount()
+        },1000)
 
-    const unpaidColumns= [
-      {
-        title: 'Last Seen',
-        dataIndex: 'lastSeen',
-        key: 'lastSeen',
-      },
-      {
-        title: 'Status',
-        dataIndex: 'status',
-        key: 'status',
-      },
-      {
-        title: 'Due Date',
-        dataIndex: 'dueDate',
-        key: 'dueDate',
-      },
-      {
-        title: 'ID',
-        dataIndex: 'id',
-        key: 'id',
-      },
-      {
-        title: 'Client',
-        dataIndex: 'client',
-        key: 'client',
-      },
-      {
-        title: 'Matter',
-        dataIndex: 'matter',
-        key: 'matter',
-      },
-      {
-        title: 'Issue Date',
-        dataIndex: 'issueDate',
-        key: 'issueDate',
-      },
-      {
-        title: 'Balance',
-        dataIndex: 'balance',
-        key: 'balance',
-      },
+      }).catch((err)=>{
+        console.log(err)
+        notification.error("Failed")
+      })
+      
+    }
+    const handleDelete = (record) => {
+      api
+        .get('/billing/bill/delete/' + record._id)
+        .then((res) => {
+          console.log(res)
+          this.componentDidMount()
+          notification.success({ message: 'Bills Deleted !' });
+          setTimeout(() => {
+            //window.location.reload();
+          }, 1500);
+        })
+        .catch((err) => {
+          notification.error({ message: 'Failed to delete' });
+        });
+    };
+
+    const columnsforDraft = [
       {
         title: 'Action',
-        dataIndex: 'status',
-        key: '7',
+        dataIndex: 'action',
+        key: 'action',
         render: (_, record) => {
           return (
             <Popconfirm
-              title="Mark as Paid"
-              onConfirm={() => this.handelAction(record._id)}
+              title="Approve this draft bill"
+              onConfirm={() => handelAction(record, "fromDraft")}
               onCancel={this.cancel}
               okText="Yes"
               cancelText="No"
@@ -171,7 +147,215 @@ class billing extends React.Component {
           );
         },
       },
+      {
+        title: 'Last Seen',
+        dataIndex: 'lastSeen',
+        key: 'lastSeen',
+      },
+      {
+        title: 'Status',
+        dataIndex: 'status',
+        key: 'status',
+      },
+      {
+        title: 'Due Date',
+        dataIndex: 'dueDate',
+        key: 'dueDate',
+      },
+      {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: 'Client',
+        dataIndex: 'client',
+        key: 'client',
+      },
+      {
+        title: 'Matter',
+        dataIndex: 'matter',
+        key: 'matter',
+      },
+      {
+        title: 'Issue Date',
+        dataIndex: 'issueDate',
+        key: 'issueDate',
+      },
+      {
+        title: 'Balance',
+        dataIndex: 'balance',
+        key: 'balance',
+      },
+      {
+        title: 'Delete',
+        dataIndex: 'Delete',
+        key: 'Delete',
+        render: (_, record) => {
+          return (
+            <Popconfirm
+              title="Are you sure you want to delete this bill ?"
+              onConfirm={() => handleDelete(record)}
+              onCancel={this.cancel}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button danger>
+                Delete
+              </Button>
+            </Popconfirm>
+          );
+        },
+      }
     ];
+
+    const unpaidColumns= [
+      {
+        title: 'Action',
+        dataIndex: 'action',
+        key: 'action',
+        render: (_, record) => {
+          return (
+            <Popconfirm
+              title="Mark as Paid"
+              onConfirm={() => handelAction(record, "fromUnpaid")}
+              onCancel={this.cancel}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Form.Check type="checkbox"  />
+            </Popconfirm>
+          );
+        },
+      },
+     
+      {
+        title: 'Last Seen',
+        dataIndex: 'lastSeen',
+        key: 'lastSeen',
+      },
+      {
+        title: 'Status',
+        dataIndex: 'status',
+        key: 'status',
+      },
+      {
+        title: 'Due Date',
+        dataIndex: 'dueDate',
+        key: 'dueDate',
+      },
+      {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: 'Client',
+        dataIndex: 'client',
+        key: 'client',
+      },
+      {
+        title: 'Matter',
+        dataIndex: 'matter',
+        key: 'matter',
+      },
+      {
+        title: 'Issue Date',
+        dataIndex: 'issueDate',
+        key: 'issueDate',
+      },
+      {
+        title: 'Balance',
+        dataIndex: 'balance',
+        key: 'balance',
+      },
+      {
+        title: 'Delete',
+        dataIndex: 'Delete',
+        key: 'Delete',
+        render: (_, record) => {
+          return (
+            <Popconfirm
+              title="Are you sure you want to delete this bill ?"
+              onConfirm={() => handleDelete(record)}
+              onCancel={this.cancel}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button danger>
+                Delete
+              </Button>
+            </Popconfirm>
+          );
+        },
+      }
+     
+    ];
+    const paidColumns= [
+      
+      {
+        title: 'Last Seen',
+        dataIndex: 'lastSeen',
+        key: 'lastSeen',
+      },
+      {
+        title: 'Status',
+        dataIndex: 'status',
+        key: 'status',
+      },
+      {
+        title: 'Due Date',
+        dataIndex: 'dueDate',
+        key: 'dueDate',
+      },
+      {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: 'Client',
+        dataIndex: 'client',
+        key: 'client',
+      },
+      {
+        title: 'Matter',
+        dataIndex: 'matter',
+        key: 'matter',
+      },
+      {
+        title: 'Issue Date',
+        dataIndex: 'issueDate',
+        key: 'issueDate',
+      },
+      {
+        title: 'Balance',
+        dataIndex: 'balance',
+        key: 'balance',
+      },
+      {
+        title: 'Delete',
+        dataIndex: 'Delete',
+        key: 'Delete',
+        render: (_, record) => {
+          return (
+            <Popconfirm
+              title="Are you sure you want to delete this bill ?"
+              onConfirm={() => handleDelete(record)}
+              onCancel={this.cancel}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button danger>
+                Delete
+              </Button>
+            </Popconfirm>
+          );
+        },
+      }
+     
+    ];
+
 
     const exportPDF = () => {
       const unit = 'pt';
@@ -216,7 +400,8 @@ class billing extends React.Component {
     };
 
     return (
-      <div className="p-2 ">
+      <Spin size= "large" spinning = {this.state.loading}>
+        <div className="p-2 ">
         <Card
           title="Billing"
           extra={
@@ -227,31 +412,34 @@ class billing extends React.Component {
               <Button onClick={() => this.handelBills('record')}>
                 Record Payment
               </Button>
+              <Button type="primary" onClick={() => this.props.history.push('/create/bills')}>
+                New Bills
+              </Button>
               {/*  <Button onClick={()=>this.showModal("expense")}>New Expense</Button>*/}
             </span>
           }
         >
           <Tabs defaultActiveKey="4" onChange={callback}>
-            {
-              /* 
-               <TabPane tab="Draft" key="1">
-              <Table dataSource={this.state.tableData} columns={columns} />;
+         
+            <TabPane tab="Draft" key="1">
+              <Table dataSource={this.state.draftBills} columns={columnsforDraft} />;
             </TabPane>
-              */
-            }
+          
            <TabPane tab="All" key="4">
-              <Table dataSource={this.state.tableData} columns={columns} />
+              <Table dataSource={this.state.tableData} columns={paidColumns} />
             </TabPane>
             <TabPane tab="Unpaid" key="3">
               <Table dataSource={this.state.unpaidBills} columns={unpaidColumns} />
             </TabPane>
             <TabPane tab="Paid" key="2">
-              <Table dataSource={this.state.paidBills} columns={columns} />
+              <Table dataSource={this.state.paidBills} columns={paidColumns} />
             </TabPane>
           </Tabs>
         </Card>
       </div>
-    );
+    
+      </Spin>
+      );
   }
 }
 
