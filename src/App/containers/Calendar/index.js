2cb3aa59915994e34e126cc66f4165ef4ab00ae0
Anas M.I <anas3rde@gmail.com>
2020-08-30 18:41:06 +0530

    index

--- src/App/containers/Calendar/index.js
+++ src/App/containers/Calendar/index.js
@@ -9,7 +9,7 @@ import { DateTimePickerComponent } from "@syncfusion/ej2-react-calendars";
 import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
 import { EventSettingsModel } from "@syncfusion/ej2-react-schedule";
 import api from '../../../resources/api'
-import {notification, Button, Card} from 'antd'
+import {notification, Button, Card, message} from 'antd'
 
 import "@syncfusion/ej2-base/styles/material.css";
 import "@syncfusion/ej2-buttons/styles/material.css";
@@ -28,16 +28,7 @@ import { getEvents } from '../../../store/Actions';
 import {  L10n, Internationalization } from '@syncfusion/ej2-base';
 
 const Add = <Button type="link">Add</Button>
-L10n.load({
-    'en-US': {
-        'schedule': {
-            'saveButton': 'Add',
-            'cancelButton': 'Close',
-            'deleteButton': 'Remove',
-            'newEvent': 'Add Event',
-        },
-    }
-});
+
 const fields = {
     id: 'Id',
     subject: { name: 'Title' },
@@ -60,21 +51,24 @@ const CalendarContainer = props => {
     //State
     const dispatch = useDispatch()
     const userId = useSelector(state=>state.user.token.user._id)
-
+    const [ID, setID] = useState("")
+    let startTime = ""
+    let endTime = ""
     const [ state, setState ] = useState({tableData : []})
-    const [ data, setData ] = useState({email : false, notification : false})
+    const [ data, setData ] = useState({email : false, notification : false, startTime : "", title: ""})
     const [listEvent, setListEvent] = useState([])
     let instance = new Internationalization();
-    useEffect(()=>{
-      async function fetchData(){
+
+    async function fetchEventData(){
+ 
         res =  await api.get('/calendar/viewforuser/'+userId)
         response =  await api.get('matter/viewforuser/'+userId)
-        const fetchEvents = await api.get('/calendar/showall')
-        setListEvent([...listEvent, ...fetchEvents.data.data])
+        setListEvent(res.data.data)
         setOptions(response.data.data)
         setdata()
       }
-      fetchData()
+    useEffect(()=>{
+      fetchEventData()
     },[])
     const setdata = () =>{
         let newTableData = []
@@ -117,9 +111,18 @@ const CalendarContainer = props => {
         if (args.type === 'Editor') {
             SchedulerRef.current.eventWindow.recurrenceEditor = recurrenceRef.current;
         }
+        /*
+        if (args.type == "QuickInfo") {
+            var dialogObj = args.element.ej2_instances[0];
+            dialogObj.hide();
+            var currentAction = args.target.classList.contains("e-work-cells") ? "Add" : "Save";
+            SchedulerRef.openEditor(args.data, currentAction);
+        }
+        */
     }
     const handleChange = (e) =>{
         e.persist()
+
         const { value, id } = e.target
         let newdata  = data
         if(id === "allday" || id==="repeat"){
@@ -139,6 +142,12 @@ const CalendarContainer = props => {
     const DateTimeChange = (e) =>{
         const { value, id} = e.element
         let newData  = data
+        if(id==="startTime" ){
+            startTime = value
+        }
+        if(id==="endTime" ){
+            endTime = value
+        }
         if(id==="matter" && e.itemData!=null){
             newData[id]=options[e.itemData.value]._id
         }else{
@@ -157,63 +166,246 @@ const CalendarContainer = props => {
           message: 'Failure'});
       };
     const handleSubmit = (e) =>{
+        console.log(e.requestType)
         if(e.requestType==="eventRemoved"){
             const id = e.data[0].id
-            api.get('/calendar/delete/'+id).then(()=>openNotificationWithIcon('success')).catch(()=>openNotificationWithfailure('error'))
+            api.get('/calendar/delete/'+id).then(()=>{
+                fetchEventData()
+                openNotificationWithIcon('success')
+            }
+           ).catch(()=>openNotificationWithfailure('error'))
             setTimeout(()=>{
-                window.location.reload()
+                //window.location.reload()
+                
             },1500)
         }
         if(e.requestType==="eventChanged"){
-            console.log(data)
-            const id = e.data.id 
+
             let eventdata = data
+            let id = e.changedRecords[0].id
+            /*
+            if(e.data.id == undefined){
+               id = e.data[0].id
+            }
+            */
             eventdata.userId = userId 
-            api.post('/calendar/update/'+id, eventdata ).then(res=>console.log(res)).then(()=>openNotificationWithIcon('success')).catch(()=>openNotificationWithfailure('error'))
+            eventdata.startTime = startTime
+            eventdata.endTime = endTime
+           
+            api.post('/calendar/update/'+ id , eventdata )
+            .then((res)=>{
+                fetchEventData()
+                notification.success({message : "Event Edited"})
+            }).catch((err)=>{
+                console.log(err)
+                notification.error({message : "Failed"})
+            })
             setData({})
             setTimeout(()=>{
-                window.location.reload()
+               // window.location.reload()
             },1500)
             
         }
         if(e.requestType==="eventCreated"){
+    
             let eventdata = data
             eventdata.userId = userId
-        api.post('/calendar/create', eventdata).then(()=>openNotificationWithIcon('success')).catch(()=>openNotificationWithfailure('error'))
-        setData({})
-        setTimeout(()=>{
-            window.location.reload()
-        },1500)
-        }
-       
+            if(startTime !== "Invalid Date"){
+    
+                eventdata.startTime = startTime
+                eventdata.endTime = endTime
+                
+            }
+            console.log(data)
+            if(data.title == "" || data.title == undefined  ){
+                    notification.warning({message : "Please provide a title" })
+            }else{
+                console.log(eventdata)
+                
+                api.post('/calendar/create', eventdata).then((res)=>{
+                    console.log(res)
+                    fetchEventData()
+                    notification.success({message : "Evented Added"})
+                 }).catch((err)=>{
+                    console.log(err)
+                    notification.error({message : "Failed!"})
+                 })
+                 setData({})
+                 setTimeout(()=>{
+                    //window.location.reload()
+                },1500)
+    
+                 }
+              
+            }
+        
     }
     
     
     const setInit = (args) =>{
-        var strDateTime = args.StartTime
-        var myDate = new Date(strDateTime);
-        const startTime = myDate.toLocaleString()
-        var strDateTime = args.EndTime
-        var myDate = new Date(strDateTime);
-        const endTime = myDate.toLocaleString()
-        setData({startTime : startTime, endTime: endTime})
-        
-    }
+    
+    
+        if(args.StartTime != undefined && args.StartTime != "Invalid Date"){
+            const props = args
+            startTime = props.StartTime
+            endTime = props.EndTime
+
+            let sdd = startTime.getDate()
+            let smm = startTime.getMonth()+1
+            let syyyy = startTime.getFullYear()
+            let shours = startTime.getHours()
+            let smins = startTime.getMinutes()
+            smins < 10 ? smins = "0"+smins : smins = smins
+
+          
+            let edd = endTime.getDate()
+            let emm = endTime.getMonth()+1
+            let eyyyy = endTime.getFullYear()
+            let ehours = endTime.getHours()
+            let emins = endTime.getMinutes()
+            emins < 10 ? emins = "0"+emins : emins = emins
+
+            
+            startTime = smm+'/'+sdd+'/'+syyyy+', '+shours+':'+smins+':00 '
+            endTime = emm+'/'+edd+'/'+eyyyy+', '+ehours+':'+emins+':00 '
+
+
+             /*
+             if((startTime.toLocaleString()).includes("AM") || (startTime.toLocaleString()).includes("PM")){
+                 console.log("Windows")
+               
+                 
+                 console.log("Mac")
+                if(startTime.getHours() > 12 || endTime.getHours() > 12){
+                    startTime.setHours(startTime.getHours()-12)
+                    startTime = startTime.toLocaleString() + " AM"
+                    endTime.setHours(endTime.getHours()-12)
+                    endTime = endTime.toLocaleString() + " AM"
+                 }else{
+                    startTime = startTime.toLocaleString() + " PM"
+                    endTime = endTime.toLocaleString() + " PM"
+                 }
+                
+                
+                
+                startTime = startTime.toLocaleString()
+                endTime = props.EndTime.toLocaleString()
+                
+             }else{
+                
+                let sdd = startTime.getDate()
+                let smm = startTime.getMonth()+1
+                let syyyy = startTime.getFullYear()
+                let sAMPM = "AM"
+                let shours = startTime.getHours()
+               // shours < 10 ? shours = "0"+shours : shours = shours
+               
+                if(shours > 12){
+                   shours = shours - 12
+                   sAMPM = "AM"
+               }else{
+                  shours = shours
+                  sAMPM = "PM"
+               }
+               
+                let smins = startTime.getMinutes()
+                smins < 10 ? smins = "0"+smins : smins = smins
    
+              
+                let edd = endTime.getDate()
+                let emm = endTime.getMonth()+1
+                let eyyyy = endTime.getFullYear()
+                let eAMPM = ""
+                let ehours = endTime.getHours()
+               // ehours < 10 ? ehours = "0"+ehours : ehours = ehours
+               
+                if(ehours > 12){
+                    ehours = ehours - 12
+                    eAMPM = "AM"
+                    
+                }else{
+                   ehours = ehours
+                   eAMPM = "PM"
+                }
+                
+                let emins = endTime.getMinutes()
+                emins < 10 ? emins = "0"+emins : emins = emins
+   
+                
+                startTime = smm+'/'+sdd+'/'+syyyy+', '+shours+':'+smins+':00 '+sAMPM
+                endTime = emm+'/'+edd+'/'+eyyyy+', '+ehours+':'+emins+':00 '+eAMPM
+   
+                console.log(startTime)
+                console.log(endTime)
+   
+                 /*
+                 console.log("Mac")
+                if(startTime.getHours() > 12 || endTime.getHours() > 12){
+                    startTime.setHours(startTime.getHours()-12)
+                    startTime = startTime.toLocaleString() + " AM"
+                    endTime.setHours(endTime.getHours()-12)
+                    endTime = endTime.toLocaleString() + " AM"
+                 }else{
+                    startTime = startTime.toLocaleString() + " PM"
+                    endTime = endTime.toLocaleString() + " PM"
+                 }
+                 
+                 
+             }
+             
+             
+             */
+        }      
+    }
+   const onClickButton2 = () =>{
+           
+    let eventdata = data
+    eventdata.userId = userId
+    if(startTime !== "Invalid Date"){
+      
+        eventdata.startTime = startTime
+        eventdata.endTime = endTime             
+    }
+  
+    if(data.title == ""){
+            notification.warning({message : "Please provide a title" })
+    }else{
+       
+        api.post('/calendar/create', eventdata).then((res)=>{
+            console.log(res)
+            //fetchEventData()
+            notification.success({message : "Evented Added"})
+            let eventData = {
+                Id: '',
+                Subject: '',
+                StartTime: '',
+                EndTime: ''
+            };
+            setData({})
+            SchedulerRef.current.openEditor(eventData, 'Add');
+         }).catch((err)=>{
+            console.log(err)
+            notification.error({message : "Failed!"})
+         })
+        
+        
+
+         }
+
+   }
    
     return (        
         <div className="row">
             <div className="col-lg-8">
-                <Card bodyStyle={{"padding" : "0px"}} 
-                extra={<span style={{ float: 'right' }}>
-                    <Button className="ml-auto" color="success" >
-                        Add Event 
-                    </Button>
-                </span>}>
-                    <ScheduleComponent height='550px' actionComplete={handleSubmit}  ref={cal=>SchedulerRef.current=cal }
-                        showQuickInfo={false} popupOpen={onPopupOpen}
+                <Card bodyStyle={{"padding" : "0px"}}>
+                    <ScheduleComponent height='550px' 
+                        actionComplete={handleSubmit}  
+                        ref={cal=>SchedulerRef.current=cal }
+                        showQuickInfo={false} 
+                        popupOpen={onPopupOpen}
                         eventSettings={{dataSource : state.tableData}}
-                        editorTemplate={pr=><EditorTemplate {...pr} userId={userId} setInit={setInit}  handleChange={handleChange} DateTimeChange={DateTimeChange} setRecurrenceRef={ref=>recurrenceRef.current=ref} />}>
+                        
+                        editorTemplate={pr=><EditorTemplate {...pr} onClickButton2={onClickButton2} setInit = {setInit(pr)} userId={userId} setInit={setInit}  handleChange={handleChange} DateTimeChange={DateTimeChange} setRecurrenceRef={ref=>recurrenceRef.current=ref} />}>
                         <ViewsDirective>
                         <ViewDirective option='Day'/>
                             <ViewDirective option='Week'/>
