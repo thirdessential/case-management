7bf749bbd7da2967af8e5ea69456ac4770056d24
Anas M.I <anas3rde@gmail.com>
2020-09-02 22:19:05 +0530

    tasks bug fix

--- src/App/containers/Tasks/index.js
+++ src/App/containers/Tasks/index.js
@@ -4,11 +4,14 @@ import UpcomingTasks from './UpcomingTasks/upcomingTasks';
 import CompletedTask from './CompletedTasks/CompletedTasks';
 import List from './List/List';
 import api from '../../../resources/api';
-import { Button, Modal, notification, Popconfirm, message } from 'antd';
+import { Button, Modal, notification, Popconfirm, message, Space, Spin } from 'antd';
 import { Form } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import jsPDF from 'jspdf';
+import TaskForm from './EditForm'
+import TaskForm2 from './Taskform'
 import { Input, Select } from 'antd';
+import ReactDOM from 'react-dom'
 import { InfoCircleOutlined, UserOutlined } from '@ant-design/icons';
 
 
@@ -18,14 +21,7 @@ let response = {};
 let ListData = null;
 let options = null;
 
-const { Option } = Select;
 
-const selectBefore = (
-  <Select defaultValue="Firm User" className="select-before">
-    <Option value="FirmUser">Firm User</Option>
-    <Option value="Contacts">Contacts</Option>
-  </Select>
-);
 
 class Tasks extends React.Component {
   constructor(props) {
@@ -34,11 +30,14 @@ class Tasks extends React.Component {
       visible: false,
       confirmLoading: false,
       loading: false,
-      Data: { priority: 'Normal', matter: '' },
+      Data: { priority: 'Normal', matter: "" },
       editMode: false,
       res: '',
       selected: null,
       status: false,
+      disable : false,
+      spinning : true,
+      options : null
     };
   }
   exportPDF = () => {
@@ -93,96 +92,23 @@ class Tasks extends React.Component {
     });
   };
 
-  handleOk = (e) => {
-    e.preventDefault();
-    notification.destroy();
-    if (
-      this.state.Data.taskName === '' ||
-      this.state.Data.taskName === undefined ||
-      this.state.Data.description === '' ||
-      this.state.Data.taskName === undefined ||
-      this.state.Data.dueDate === '' ||
-      this.state.Data.dueDate === undefined ||
-      this.state.Data.matter === '' ||
-      this.state.Data.matter === undefined
-    ) {
-      console.log(this.state.Data);
-
-      return notification.warning({
-        message: 'Fields Should Not Be Empty',
-      });
-    } else {
-      this.setState({
-        confirmLoading: true,
-      });
-      const data = this.state.Data;
-      data.userId = this.props.userId;
-      if (this.state.editMode) {
-        api
-          .post('tasks/edit/' + this.state.selected, data)
-          .then(() => this.openNotificationWithSucces('success'))
-          .catch(() => {
-            this.openNotificationWithFailure('error');
-          });
-      } else {
-        api
-          .post('/tasks/create', data)
-          .then(() => this.openNotificationWithSucces('success'))
-          .catch(() => {
-            this.openNotificationWithFailure('error');
-          });
-      }
-      setTimeout(() => {
-        this.setState({
-          visible: false,
-          confirmLoading: false,
-        });
-        window.location.reload();
-      }, 1000);
-    }
-  };
-
-  handelAction = (_id) =>{
-    this.setState({
-      status : true
-    })
-    const data = this.state.status;
-    console.log(data)
-    api.get('/tasks/updatetask/'+_id, data)
-    .then((res) => {
-      console.log(res)
-    })
-    .catch((err) => {
-        console.log(err); 
-      });
-  }
-
-  handelNonAction = (_id) =>{
-    this.setState({
-      status : false
-    })
-    const data = this.state.status;
-    console.log(data)
-    api.get('/tasks/updatetask/'+_id, data)
-    .then((res) => {
-      console.log(res)
-    })
-    .catch((err) => {
-        console.log(err); 
-      });
-  }
 
   handleCancel = () => {
     this.setState({
       visible: false,
+      editMode : false,
+      Data: { priority: 'Normal', matter: "" },
     });
   };
 
   handleChange = (e) => {
     e.persist();
     let newState = this.state;
-    if (e.target.id === 'matter') {
-      newState.Data[e.target.id] = response[e.target.selectedIndex];
+    if (e.target.id === "matter" ) {
+      if( e.target.selectedIndex > 0){
+        newState.Data[e.target.id] = response[e.target.selectedIndex - 1];
+      }
+      
     } else {
       newState.Data[e.target.id] = e.target.value;
     }
@@ -190,20 +116,9 @@ class Tasks extends React.Component {
     console.log(this.state);
   };
 
-  EditHandler(_id) {
-    this.setState({ editMode: true });
-    this.setState({ res: _id });
-    this.setState({ selected: _id });
-    this.showModal();
-  }
+  
 
-  deleteHandler(_id) {
-    api.get('tasks/delete/' + _id);
-    message.success('Deleted');
-    setTimeout(() => {
-      window.location.reload();
-    }, 1500);
-  }
+ 
   getISTDate(dateInUTC) {
     var localDate = new Date(dateInUTC);
     return localDate.toLocaleString();
@@ -216,26 +131,53 @@ class Tasks extends React.Component {
     this.props.history.push('/tasks/view/list', record)
   };
   async componentDidMount() {
+    if(this.props.location.state === "from dashboard"){
+      this.showModal()
+    }else{
+      this.setState({
+        visible : false
+      })
+    }
     let tableData = [];
-    await api
+    let CompletedData = []
+    let upcomingData = []
+
+    api
       .get('/matter/viewforuser/' + this.props.userId)
-      .then((res) => (response = res.data.data));
-    console.log(response);
-    options = response.map((value, index) => {
-      if (index == 0) {
-        let newdata = this.state;
-        newdata.Data.matter = value._id;
-        this.setState(newdata);
-      }
+      .then((res) =>{
+        response = res.data.data
+        console.log(response);
+        options = response.map((value, index) => {
+      
       return <option>{value.matterDescription}</option>;
+      } );
+       
     });
 
     await api.get('/tasks/viewforuser/' + this.props.userId).then((res) => {
-      console.log(res.data.data);
-      const newdata = res.data.data.filter(function( obj ) {
-        return obj.status == false;});
-      tableData = [ ...tableData, ...newdata]  
+      console.log(res)
+      res.data.data.map((value, index)=>{
+        const newdata = value
+        newdata.key = index 
+        newdata.dueDate = newdata.dueDate ? newdata.dueDate.substring(0,10)  : "-"
+        if(value.status == false){
+          
+          upcomingData.push(newdata)
+        }
+        if(value.status == true){
 
+          CompletedData.push(newdata)
+        }
+        tableData.push(newdata)
+        
+      })
+      this.setState({ 
+         CompletedData, 
+         tableData,
+         options ,
+         upcomingData,
+        spinning: false
+       });
       // res.data.data.map((item, index) => {
       //   tableData = [
       //     ...tableData,
@@ -245,7 +187,7 @@ class Tasks extends React.Component {
       //     },
       //   ];
       // });
-
+/*
       ListData = res.data.data.map((value, index) => {
         return (
           <tr>
@@ -271,9 +213,10 @@ class Tasks extends React.Component {
             </td>
           </tr>
         );
-      });
+      });*/
     });
-    this.setState({ ListData, tableData, options });
+    
+
   }
 
   columns = [
@@ -302,9 +245,7 @@ class Tasks extends React.Component {
       key: '3',
       sortDirections: ['descend', 'ascend'],
       sorter: (a, b) => a.dueDate.length - b.dueDate.length,
-      render: (_, record) => {
-        return this.getISTDate(record.dueDate);
-      },
+      
     },
     {
       title: 'Action',
@@ -330,7 +271,7 @@ class Tasks extends React.Component {
       key: '6',
       render: (_, record) => {
         return (
-          <Button onClick={() => this.EditHandler(record._id)}>Edit</Button>
+          <Button onClick={() => this.EditHandler(record)}>Edit</Button>
         );
       },
     },
@@ -353,6 +294,134 @@ class Tasks extends React.Component {
       },
     },
   ];
+  deleteHandler(_id) {
+    api.get('tasks/delete/' + _id).then((res)=>{
+      console.log(res)
+      notification.success({message : "Task Deleted"})
+      this.componentDidMount()
+    })
+   
+  }
+  EditHandler(_id) {
+    this.setState({ editMode: true });
+    this.setState({ Data: _id });
+    console.log(this.state.Data)
+    this.setState({ selected: _id });
+  }
+  handleOk = (e) => {
+    e.persist();
+    notification.destroy();
+    let valid = true
+    console.log(this.state.Data.matter)
+    if(this.state.Data.taskName === '' || this.state.Data.taskName === undefined ){
+      valid = false
+      notification.warning({
+        message: 'Please provide a taskName',
+      });
+    }
+    if(this.state.Data.description === '' ||this.state.Data.description === undefined  ){
+      valid = false
+      notification.warning({
+        message: 'Please provide a description',
+      });
+    }
+    if( this.state.Data.dueDate === '' || this.state.Data.dueDate === undefined  ){
+      valid = false
+      notification.warning({
+          message: 'Please select a due date',
+        });
+    }
+    if( this.state.Data.matter === "" || this.state.Data.matter === undefined ){
+      valid = false
+      notification.warning({
+        message: 'Please select a matter',
+      });
+      
+    } 
+    if(valid){
+      this.setState({
+        confirmLoading: true,
+        disable : true
+      });
+      const data = this.state.Data;
+      data.userId = this.props.userId;
+      if (this.state.editMode) {
+        api
+          .post('tasks/edit/' + data._id, data)
+          .then((res) => {
+            console.log(res)
+            this.componentDidMount()
+            notification.success({message : "Task Edited"})
+          }
+          )
+          .catch(() => {
+            this.openNotificationWithFailure('error');
+          });
+      } else {
+        api
+          .post('/tasks/create', data)
+          .then((res) => {
+            console.log(res)
+            this.openNotificationWithSucces('success')
+
+            this.componentDidMount()
+          })
+          .catch(() => {
+            this.openNotificationWithFailure('error');
+          }).then(()=>{
+            ReactDOM.findDOMNode(this.messageForm).reset()
+          })
+      }
+      
+        setTimeout(() => {
+          this.setState({
+            visible: false,
+            editMode : false,
+            disable : false,
+            Data: { priority: 'Normal', matter: "" },
+            confirmLoading: false,
+          });
+          //window.location.reload();
+        }, 1000);
+      
+    }
+      
+  };
+
+  handelAction = (_id) =>{
+    this.setState({
+      status : true
+    })
+    const data = this.state.status;
+    console.log(data)
+    api.get('/tasks/updatetask/'+_id, data)
+    .then((res) => {
+      this.componentDidMount()
+      notification.success({message : "Marked as completed"})
+      console.log(res)
+    })
+    .catch((err) => {
+        console.log(err); 
+      });
+   setTimeout(()=>{
+    //window.location.reload()
+   },600)
+  }
+  handelNonAction = (record) =>{
+    const data = record
+    data.status = false
+    console.log(data)
+    api.get('/tasks/setfalse/' + record._id, data)
+    .then((res) => {
+      console.log(res)
+      this.componentDidMount()
+      notification.success({message : "Unmarked as completed"})
+    })
+    .catch((err) => {
+        console.log(err); 
+      });
+  }
+
 
   newcolumns = [
     {
@@ -380,9 +449,7 @@ class Tasks extends React.Component {
       key: '3',
       sortDirections: ['descend', 'ascend'],
       sorter: (a, b) => a.dueDate.length - b.dueDate.length,
-      render: (_, record) => {
-        return this.getISTDate(record.dueDate);
-      },
+      
     },
     {
       title: 'Action',
@@ -392,7 +459,7 @@ class Tasks extends React.Component {
         return (
           <Popconfirm
             title="Mark as Incomplete"
-            onConfirm={() => this.handelNonAction(record._id)}
+            onConfirm={() => this.handelNonAction(record)}
             onCancel={this.cancel}
             okText="Yes"
             cancelText="No"
@@ -408,7 +475,7 @@ class Tasks extends React.Component {
       key: '6',
       render: (_, record) => {
         return (
-          <Button onClick={() => this.EditHandler(record._id)}>Edit</Button>
+          <Button onClick={() => this.EditHandler(record)}>Edit</Button>
         );
       },
     },
@@ -431,8 +498,9 @@ class Tasks extends React.Component {
       },
     },
   ];
-
+  
   render() {
+    console.log(this.state.tableData)
     const operations = (
       <span>
         <Button className="ml-auto" color="success" onClick={this.exportPDF}>
@@ -447,7 +515,8 @@ class Tasks extends React.Component {
     }
 
     return (
-      <div>
+      <Spin size="large" spinning={this.state.spinning} >
+        <div>
         <Tabs
           defaultActiveKey="1"
           tabBarExtraContent={operations}
@@ -457,13 +526,13 @@ class Tasks extends React.Component {
           <TabPane tab="Upcoming Tasks" key="1">
             <UpcomingTasks
               columns={this.columns}
-              tableData={this.state.tableData}
+              tableData={this.state.upcomingData}
             />
           </TabPane>
           <TabPane tab="Completed Tasks" key="2">
             <CompletedTask
               columns={this.newcolumns}
-              tableData={this.state.tableData}
+              tableData={this.state.CompletedData}
             />
           </TabPane>
           <TabPane tab="List" key="3">
@@ -471,87 +540,130 @@ class Tasks extends React.Component {
           </TabPane>
         </Tabs>
         <Modal
-          title="Add to New Task"
+          title="Create New Task"
           visible={this.state.visible}
           confirmLoading={this.state.confirmLoading}
           onCancel={this.handleCancel}
           onOk={this.handleOk}
+          footer={[
+            <Button  onClick={this.handleCancel}>
+              Cancel
+            </Button>,
+            <Button type="primary" disabled = {this.state.disable} onClick={this.handleOk}>
+              Create Task
+            </Button>,
+          ]}
         >
-          <Form className="form-details">
-            <Form.Group controlId="taskName">
-              <Form.Label>Task Name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Task Name"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
+       <Form 
+       className="form-details" 
+       id='myForm'
+       className="form"
+       ref={ form => this.messageForm = form }
+       >
+        <Form.Group controlId="taskName">
+          <Form.Label>Task Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Task Name"
+            defaultValue={this.state.name}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
 
-            <Form.Group controlId="dueDate">
-              <Form.Label>Due Date</Form.Label>
-              <Form.Control
-                required
-                type="date"
-                placeholder="Due Date"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
+        <Form.Group controlId="dueDate">
+          <Form.Label>Due Date</Form.Label>
+          <Form.Control
+            required
+            type="date"
+            placeholder="Due Date"
+            onChange={this.handleChange}
+          />
+        </Form.Group>
 
-            <Form.Group controlId="description">
-              <Form.Label>Description</Form.Label>
-              <Form.Control
-                required
-                as="textarea"
-                rows="3"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
+        <Form.Group controlId="description">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            required
+            as="textarea"
+            rows="3"
+            onChange={this.handleChange}
+          />
+        </Form.Group>
 
-            <Form.Group controlId="taskName">
-              <Form.Label>Assignee</Form.Label>
-              <div>
-                <Input addonBefore={selectBefore} size="large" suffix={<UserOutlined className="site-form-item-icon" />}  placeholder="Type a name..." />
-              </div>
-            </Form.Group>
+      {/*
+        <Form.Group controlId="taskName">
+          <Form.Label>Assignee</Form.Label>
+          <div>
+            <Input addonBefore={selectBefore} size="large" suffix={<UserOutlined className="site-form-item-icon" />}  placeholder="Type a name..." />
+          </div>
+        </Form.Group>
+      */
+      }
 
-            <Form.Group controlId="priority">
-              <Form.Label>Priority</Form.Label>
-              <Form.Control
-                as="select"
-                defaultValue="Normal"
-                required
-                onChange={this.handleChange}
-              >
-                <option>Low</option>
-                <option>Normal</option>
-                <option>High</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="matter">
-              <Form.Label>Matter</Form.Label>
-              <Form.Control
-                required
-                as="select"
-                onChange={this.handleChange}
-                name="matter"
-              >
-                {this.state.options}
-              </Form.Control>
-            </Form.Group>
-            <br />
+        <Form.Group controlId="priority">
+          <Form.Label>Priority</Form.Label>
+          <Form.Control
+            as="select"
+            defaultValue="Normal"
+            required
+            onChange={this.handleChange}
+          >
+            <option>Low</option>
+            <option>Normal</option>
+            <option>High</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="matter">
+          <Form.Label>Matter</Form.Label>
+          <Form.Control
+            required
+            as="select"
+            onChange={this.handleChange}
+            name="matter"
+          >
+            <option>Select a matter</option>
+            {this.state.options}
+          </Form.Control>
+        </Form.Group>
+        <br />
+        {
+          /*
             <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Notify me when the task is completed" />
-            </Form.Group>
-            <br />
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Notify assignee via email" />
-            </Form.Group>
-            <br />
-          </Form>
+          <Form.Check type="checkbox" label="Notify me when the task is completed" />
+        </Form.Group>
+        <br />
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Notify assignee via email" />
+        </Form.Group>
+        <br />
+          */
+
+        }
+       
+      </Form>
+   
+        </Modal>
+        <Modal
+          title="Edit task"
+          visible={this.state.editMode}
+          confirmLoading={this.state.confirmLoading}
+          onCancel={this.handleCancel}
+          onOk={this.handleOk}
+          footer={[
+            <Button  onClick={this.handleCancel}>
+              Cancel
+            </Button>,
+            <Button type="primary" disabled = {this.state.disable} onClick={this.handleOk}>
+              Update Task
+            </Button>,
+          ]}
+        >
+          <TaskForm options={this.state.options} data={this.state.Data}  handleChange={this.handleChange}></TaskForm>
         </Modal>
       </div>
-    );
+    
+      </Spin>
+      );
   }
 }
 
